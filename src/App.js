10852c1281ee9ab8b4a8d7733685c64722323abf
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-09-10 15:18:34 -0500

    FIX Aviso Cofepris

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ function App() {
               supuesto, se encontrarán ligeras diferencias en las cifras si se
               comparan ambos, pero en general, escasamente significativas. La
               valoración es también similar: <sup>2</sup>
-              
             </p>
             <section className="tabla">
               <table>
@@ -59,28 +58,27 @@ function App() {
               </table>
             </section>
             <div className="frase">
-            <p >
-              <strong>
-                La dosis recomendada de Halaven<sup>®</sup> es 1.4 mg/m<sup>2</sup>{" "}
-              </strong>
-               debe ser administrada por vía intravenosa durante 2 a 5 minutos los días 1 y 8 de cada ciclo de 21 días.<sup>3</sup>
-            
-            </p>
-            
+              <p>
+                <strong>
+                  La dosis recomendada de Halaven<sup>®</sup> es 1.4 mg/m
+                  <sup>2</sup>{" "}
+                </strong>
+                debe ser administrada por vía intravenosa durante 2 a 5 minutos
+                los días 1 y 8 de cada ciclo de 21 días.<sup>3</sup>
+              </p>
             </div>
-           
-           
+
             <small>
               <strong>Referencias:</strong>
             </small>
             <small>
-            1.- Dubois D, Dubois EF. A formula to estimate the approximate surface
-              area if height and weight be known. Arch Intern Med. 1916;
+              1.- Dubois D, Dubois EF. A formula to estimate the approximate
+              surface area if height and weight be known. Arch Intern Med. 1916;
               17:863-871.
             </small>
             <small>
-            2.- Wang Y., Moss, J, Thisted R. Predictors of Body Surface Area. J
-              Clin Anesth 1992; 4(1): 4-10.
+              2.- Wang Y., Moss, J, Thisted R. Predictors of Body Surface Area.
+              J Clin Anesth 1992; 4(1): 4-10.
             </small>
             <small>3.- Halaven-IPPA-193300423M0029-02dic2019.</small>
           </div>
@@ -88,14 +86,18 @@ function App() {
             <p className="titulo">fórmulas</p>
             <div className="contenedor-formulas">
               <div className="formula">
-                <p>Fórmula de DuBois y DuBois: <sup>1</sup></p>
+                <p>
+                  Fórmula de DuBois y DuBois: <sup>1</sup>
+                </p>
                 <strong>
                   BSA = 0.007184 x Peso <sup>0.425</sup> x altura{" "}
-                  <sup>0.725</sup> 
+                  <sup>0.725</sup>
                 </strong>
               </div>
               <div className="formula">
-                <p>Fórmula para determinar dosis: <sup>2</sup></p>
+                <p>
+                  Fórmula para determinar dosis: <sup>2</sup>
+                </p>
                 <strong>
                   {" "}
                   Dosis = 1.4 mg/m<sup>2</sup>{" "}
@@ -110,10 +112,26 @@ function App() {
           <img src={LogoHalven} alt="logo-halaven" />
         </section>
         <div className="color"></div>
-        <small className="crc"><span>CRC:</span> LEN728-21. <span>No. Cofepris:</span> 213300202C8894/213300202C8897.</small>
-       <small  className="legales" >Toda la información contenida y generada por el sistema de Eisai Laboratorios S. de R.L. de C.V. se ofrece únicamente para fines educativos.</small>
-      <small className="legales">Esta información no debe emplearse para el diagnóstico o tratamiento de ninguna enfermedad o problema de salud.</small>
-      <small className="legales"><strong>EL PROPÓSITO DE ESTA INFORMACIÓN NO ES SUSTITUIR EL CRITERIO CLÍNICO NI ORIENTAR SOBRE EL TRATAMIENTO DE PACIENTES INDIVIDUALES DE NINGÚN MODO.</strong></small>
+        <small className="crc">
+          <span>CRC:</span> LEN728-21. <span>No. Cofepris:</span>{" "}
+          213300202C8894/213300202C8897.
+        </small>
+        <small className="legales">
+          Toda la información contenida y generada por el sistema de Eisai
+          Laboratorios S. de R.L. de C.V. se ofrece únicamente para fines
+          educativos.
+        </small>
+        <small className="legales">
+          Esta información no debe emplearse para el diagnóstico o tratamiento
+          de ninguna enfermedad o problema de salud.
+        </small>
+        <small className="legales">
+          <strong>
+            EL PROPÓSITO DE ESTA INFORMACIÓN NO ES SUSTITUIR EL CRITERIO CLÍNICO
+            NI ORIENTAR SOBRE EL TRATAMIENTO DE PACIENTES INDIVIDUALES DE NINGÚN
+            MODO.
+          </strong>
+        </small>
       </div>
     </div>
   );
